7042faa5c579a57fc1a5382d18f95b935d35680d
Frijolero1 <villah405@gmail.com>
2025-07-01 21:28:19 -0700

    I change the display massage

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!");
+        Console.WriteLine("I am getting board when it doesn't work");
     }
 }
